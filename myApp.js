f1719f529ffc3772e32cdc3b911544adbe1428ca
Colm Sheehan <colms152@gmail.com>
2020-04-05 12:42:48 +0100

    fix

--- myApp.js
+++ myApp.js
@@ -41,7 +41,7 @@ app.use(express.static(__dirname + "/public"));
 var response
 console.log("test that this is printing");
 console.log(process.env);
-if (process.env.MESSAGE_STYLE === "uppercase") {  
+if (file.env.MESSAGE_STYLE === "uppercase") {  
   response = "HELLO JSON".toUpperCase();
 } else {
   response = "Hello json";
